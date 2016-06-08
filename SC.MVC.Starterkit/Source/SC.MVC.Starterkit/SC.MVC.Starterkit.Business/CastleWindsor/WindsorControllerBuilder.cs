79ccc1a59447a3a69b46f8dfb39170c88e47d45d
zencqui <arsenmyutuc@hotmail.com>
2016-06-08 20:17:15 +0800

    adding controllerbuilder and renderer

--- SC.MVC.Starterkit/Source/SC.MVC.Starterkit/SC.MVC.Starterkit.Business/CastleWindsor/WindsorControllerBuilder.cs
+++ SC.MVC.Starterkit/Source/SC.MVC.Starterkit/SC.MVC.Starterkit.Business/CastleWindsor/WindsorControllerBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using Sitecore.Mvc.Controllers;
+using Sitecore.Mvc.Helpers;
 using Sitecore.Mvc.Presentation;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Mvc;
+using Sitecore.StringExtensions;
 
 namespace SC.MVC.Starterkit.Business.CastleWindsor
 {
@@ -35,12 +37,30 @@ namespace SC.MVC.Starterkit.Business.CastleWindsor
             return windsorControllerFactory.CreateController(context.RequestContext, this.ControllerName);
         }
 
-        //protected override IController CreateControllerUsingReflection()
-        //{
-        //    string controllerSpecification = this.ControllerSpecification;
+        protected override IController CreateControllerUsingReflection()
+        {
+            string controllerSpecification = this.ControllerSpecification;
+            object @object = TypeHelper.CreateObject(controllerSpecification, new object[0]);
 
-        //}
+            if(@object == null)
+            {
+                throw new InvalidOperationException("Could not create a controller instance from type name: '{0}'".FormatWith(new object[]
+				{
+					controllerSpecification
+				}));
+            }
 
-       
+            var controller = @object as IController;
+            if(controller == null)
+            {
+                throw new InvalidOperationException("The controller instance does not implement '{0}'. Controller type: '{1}'".FormatWith(new object[]
+				{
+					typeof(IController),
+					@object.GetType()
+				}));
+            }
+
+            return controller;
+        }
     }
 }
