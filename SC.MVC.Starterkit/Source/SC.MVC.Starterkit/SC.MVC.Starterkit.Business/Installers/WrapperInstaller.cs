2430aab251d77baf85b3d39b07805d1bcc920cf8
zencqui <arsenmyutuc@hotmail.com>
2016-06-24 15:26:02 +0800

    adding sample controller rendering with datasource

---
+++ SC.MVC.Starterkit/Source/SC.MVC.Starterkit/SC.MVC.Starterkit.Business/Installers/WrapperInstaller.cs
@@ -0,0 +1,29 @@
+﻿using Castle.MicroKernel.Registration;
+using SC.MVC.Starterkit.Business.CastleWindsor;
+using SC.MVC.Starterkit.Business.Wrappers;
+using Sitecore.Mvc.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SC.MVC.Starterkit.Business.Installers
+{
+    [AutoInstall]
+    public class WrapperInstaller : IWindsorInstaller
+    {
+        public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<RenderingContextWrapperBase>()
+                .UsingFactoryMethod(() => new RenderingContextWrapper(RenderingContext.Current))
+                .LifestyleTransient());
+
+            container.Register(
+                Component.For<RenderingWrapperBase>()
+                .ImplementedBy<RenderingWrapper>()
+                .LifestyleTransient());
+        }
+    }
+}
---
+++ SC.MVC.Starterkit/Source/SC.MVC.Starterkit/SC.MVC.Starterkit.Mvc/Areas/SCMVC/ViewModels/CopyrightViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SC.MVC.Starterkit.Mvc.Areas.SCMVC.ViewModels
+{
+    public class CopyrightViewModel
+    {
+        public string CopyrightText { get; set; }
+    }
+}
